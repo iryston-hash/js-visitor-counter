f8f719c1d44602bcbe3d9d6d227d06a0e1b227b4
Iryston <81931943+iryston-hash@users.noreply.github.com>
2022-02-01 15:39:38 +0200

    Add files via upload

---
+++ js visitor counter/src/js/main.js
@@ -0,0 +1,120 @@
+// document.getElementById("count-el").innerText = 10;
+
+// function increment() {
+//     console.log('click positive');
+// }
+
+
+// let lap = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// function laps() {
+//     console.log(lap + lap2 + lap3);
+// }
+// laps();
+
+
+
+// let lapsCompleted = 0;
+// function incrementLap() {
+//     lapsCompleted = lapsCompleted + 1;  
+    
+// }
+
+// console.log(lapsCompleted);
+
+// let countEl = document.getElementById('count-el');
+
+// let count = 0;
+// function increment() {
+//     count = count + 1;
+//     countEl.innerText = count;
+//     console.log(count);
+    
+
+
+// let countEl = document.getElementById('count-el');
+// let count = 0;
+
+// function increment() {
+//     count ++;
+//     countEl.innerText = count;
+// }
+
+// function save() {
+//     console.log(count);
+// }
+// save();
+
+
+// const username = "alan";
+// let msg = 'You have three new notifications';
+// let msgToUser = msg + ", " + username + '!';
+// console.log(msgToUser);
+
+
+
+// let username = 'alan';
+// let greeting = 'Hi , my name is';
+// let myGreeting = greeting + ' , ' + username;
+// console.log(myGreeting);
+
+
+// let welcomeEl = document.getElementById("welcome-el");
+// let username = 'alan';
+// let greeting = 'Welcome back, ';
+
+// welcomeEl.innerText = greeting + username;
+
+// welcomeEl.innerText +=" 😻";
+
+// let welcomeEl = document.getElementById("welcome-el");
+// let greeting = "Nice to have you again , ";
+// let username = "Arman";
+// welcomeEl.innerText = greeting + username;
+// welcomeEl.innerText += " :)";
+
+// let countEl = document.getElementById("count-el");
+// let count = 0;
+//     function increment() {
+//         count ++;
+//         countEl.innerText = count;
+//     }
+
+//     let saveEl = document.getElementById("save-el");
+//     function save() {
+//         console.log(count);
+//     }
+//     save();
+
+// let countSave = count + " -";
+// saveEl.innerText = countSave;
+// 
+// 
+// 
+
+// welcomeEl = document.getElementById("welcome-el");
+// countEl = document.getElementById("count-el");
+// saveEl = document.getElementById("save-el");
+
+// let greeting = "Welcome ,";
+// let username = "Arman";
+// let userGreeting = greeting + username;
+// welcomeEl.innerText = userGreeting;
+
+// let count = 0;
+// function increment() {
+//     count ++;
+//     countEl.innerText = count;
+// }
+
+// function save() {
+//     let countStr = count + " - ";
+//     saveEl.textContent += countStr;
+//     countEl.innerText = 0;
+//     count = 0;
+// }
+
+
+
